c2aeec369ebef440c1dda604edf7987bbda561a0
emifitzy <efitz@bu.edu>
2019-11-12 21:58:12 -0500

    added missing directories and added a requirements file for easy use

--- src/mover.py
+++ src/mover.py
@@ -44,13 +44,13 @@ for i in range(len(annotations)):
         diffs[i,:]=0
 
 
-    
+
 sze=192
 check=[]
 # %% blur annotations, retaining same count
 for i in range(len(gt)):
     check.append(sum(sum((gt[i]))))
-    
+
 
 # %% get means for all
 meanIm=np.mean([x for x in ims])
@@ -59,13 +59,16 @@ meanGT=np.mean([x for x in gt])
 sdevGt=np.std([x for x in gt])
 maxGT=np.max([x for x in gt])
 
-    
+
 # %% deposit all
 print('deposit all')
 count=[0,0,0]
 
 #chdir+delete
-os.chdir(home + '/data/all')
+os.chdir(home + '/data')
+if not os.path.exists('all'):
+    os.mkdir('all')
+os.chdir('all')
 files = glob.glob('*')
 for f in files:
     os.remove(f)
@@ -81,18 +84,18 @@ for i in range(len(c)):
     elif  c[i]==3:
         first='b-rats'
         count[2]=count[2]+1
-        
+
     name=first+"_%03d"%count[c[i]-1]
     nameList.append(name)
-    
+
     im_this=ims[i]
     gt_this=gt[i]
-    
+
     #print(i/len(c))
 
 # %% randomize along previously used vec
 if True:
-    
+
     c=c[randVec]
     ims=ims[randVec]
     gt=gt[randVec]
@@ -105,7 +108,7 @@ check=[]
 # %% blur annotations, retaining same count
 for i in range(len(gt)):
     check.append(sum(sum((gt[i]))))
-    
+
 
 # %% deposit test/train split
 
@@ -123,72 +126,81 @@ testNameList=[nameList[i+int(n*p)] for i in range(int(1+n*(1-p)))]
 # %%depost train
 print('deposit train')
 os.chdir('..')
+if not os.path.exists('train'):
+    os.mkdir('train')
 os.chdir('train')
+if not os.path.exists('image'):
+    os.mkdir('image')
 files = glob.glob('image/*')
 for f in files:
     os.remove(f)
+if not os.path.exists('label'):
+    os.mkdir('label')
 files = glob.glob('label/*')
 for f in files:
     os.remove(f)
-    
+
+os.chdir('image')
 for i in range(len(trainC)):
-            
+
     name=trainNameList[i]
     im_this=trainIms[i]
-    gt_this=trainGt[i]
-    
-    np.save('image\\' + name + '.npy',im_this)
-    np.save('label\\'+name + '.npy', gt_this)
+
+    np.save(name + '.npy',im_this)
     #im = Image.fromarray(A)
     #im.save("your_file.jpeg")
     print(i/len(trainC))
 
+os.chdir('../label')
+for i in range(len(trainC)):
+
+    name=trainNameList[i]
+    gt_this=trainGt[i]
+
+    np.save(name + '.npy', gt_this)
+    #im = Image.fromarray(A)
+    #im.save("your_file.jpeg")
+    print(i/len(trainC))
+
+os.chdir('..')
 # %%depost test
 print('deposit test')
 os.chdir('..')
-os.chdir('test')    
+if not os.path.exists('test'):
+    os.mkdir('test')
+os.chdir('test')
+if not os.path.exists('image'):
+    os.mkdir('image')
 files = glob.glob('image/*')
 for f in files:
     os.remove(f)
+if not os.path.exists('gt'):
+    os.mkdir('gt')
 files = glob.glob('gt/*')
 for f in files:
     os.remove(f)
+if not os.path.exists('pred'):
+    os.mkdir('pred')
 files = glob.glob('pred/*')
 for f in files:
     os.remove(f)
-    
+
+os.chdir('image')
 for i in range(len(testC)):
-            
+
     name=testNameList[i]
     im_this=testIms[i]
+    name='test_' + str('%03d' % i)
+    np.save(name + '.npy',im_this)
+    print(i/len(testC))
+
+os.chdir('../gt')
+for i in range(len(testC)):
+
+    name=testNameList[i]
     gt_this=testGt[i]
     name='test_' + str('%03d' % i)
-    np.save('image\\' + name + '.npy',im_this)
-    np.save('gt\\'+name + '.npy' , gt_this)    
+    np.save(name + '.npy' , gt_this)
     print(i/len(testC))
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+os.chdir('..')
